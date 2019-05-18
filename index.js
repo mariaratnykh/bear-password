81d3be61b8b1c84f0d5e375b6ea794f9f25f995e
Maria Ratnykh <mariaratnykh@ya.ru>
2019-05-18 15:58:20 +0300

    Add moving for a bear

---
+++ index.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('input');
+const bearMovingParts = [document.querySelector('.nose'), document.querySelector('.eyes'), document.querySelector('.jaws')];
+
+input.addEventListener('input', (evt) => {
+  const style = createBearStyles (evt.target.value);
+  bearMovingParts.forEach(bearOrgan => setStyles(bearOrgan, style));
+})
+
+function setStyles(elem, styles) {
+  Object.entries(styles).forEach(styleItem => {
+    elem.style[styleItem[0]] = styleItem[1];
+  })
+}
+
+function createBearStyles (inputString) {
+  if(inputString.length === 0) return { transform: ''};
+  if(inputString.length < 12) {
+    return {
+      transform: `translateX(-${12 - inputString.length}px) translateY(5px)`,
+    }
+  }
+  if(inputString.length === 12) {
+    return {
+      transform: `translateX(0px) translateY(5px)`,
+    }
+  }
+  if(inputString.length > 12) {
+    return {
+      transform: `translateX(${inputString.length - 12}px) translateY(5px)`,
+    }
+  }
+}
